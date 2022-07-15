6d8601eb4a570eea9c5b05d9240eba138b0bc00b
kapiosk <29824083+kapiosk@users.noreply.github.com>
2022-07-15 16:19:53 +0300

    init

---
+++ DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+
+namespace _3ai.solutions.PDFClient
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPDFHttp(this IServiceCollection services, string baseAddress, string licence)
+        {
+            services.AddHttpClient<PDFHttpClientService>(
+                    o =>
+                    {
+                        o.BaseAddress = new(baseAddress);
+                        o.DefaultRequestHeaders.Add("licence", licence);
+                    })
+                    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+                    {
+                        AutomaticDecompression = DecompressionMethods.All,
+                    });
+            return services;
+        }
+    }
+}
---
+++ PDFHttpClientService.cs
@@ -0,0 +1,27 @@
+﻿namespace _3ai.solutions.PDFClient
+{
+    public class PDFHttpClientService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PDFHttpClientService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<byte[]> GetPDFAsync(
+            string url,
+            string token = "",
+            bool landscape = false,
+            CancellationToken cancellationToken = default)
+        {
+            if (!string.IsNullOrEmpty(token))
+                _httpClient.DefaultRequestHeaders.Add("Authorization", token);
+            if (landscape)
+                _httpClient.DefaultRequestHeaders.Add("landscape", "true");
+
+            var response = await _httpClient.GetAsync($"PDFURL?dataUrl={url}", cancellationToken);
+            return await response.Content.ReadAsByteArrayAsync(cancellationToken);
+        }
+    }
+}
